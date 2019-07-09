d61dae19b0c2c8a29f05d34f76b5ab62555e2ea5
emjogal <emmajgale@hotmail.com>
2019-07-09 21:09:17 +0100

    adding correct js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,64 +1,102 @@
-body {
-  background-color: #010165;
-  margin: 0px;
-}
+let doorImage1 = document.getElementById("door1");
+let doorImage2 = document.getElementById("door2");
+let doorImage3 = document.getElementById("door3");
+let startButton = document.getElementById("start");
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 
-header {
-  background-color: #00ffff;
-  text-align: center;
-}
+let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 
-.title-row {
-  margin-top: 42px;
-  margin-bottom: 21px;
-  text-align: center;
-}
+let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+let closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
+let numClosedDoors = 3; 
+let openDoor1;
+let openDoor2;
+let openDoor3;
+let currentlyPlaying = true;
 
-.instructions-title {
-  display: inline;
-  font-size: 18px;
-  color: #00ffff;
-  font-family: "Work Sans";
-}
+const isBot = (door) => {
+  if (door.src === botDoorPath){
+    return true;  
+  } else {
+    return false;
+  };
+};
 
-.instructions-row {
-  margin: 0 auto;
-  width: 400px;
-}
+const isClicked = (door) => {
+  if (door.src === closedDoorPath){
+    return false;  
+  }else{
+    return true;
+  };
+};
+const playDoor = (door) => {
+  numClosedDoors --;
+  if (numClosedDoors === 0){
+    gameOver("win");
+  } else if (isBot(door)){
+    gameOver();
+  }
+};
+const randomChoreDoorGenerator = () => {
+  let choreDoor = Math.floor(Math.random()*numClosedDoors);
+  if (choreDoor === 0){
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  }
+  else if (choreDoor === 1){
+    openDoor2 = botDoorPath;
+    openDoor1 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  }
+  else if (choreDoor === 2){
+    openDoor3 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor1 = spaceDoorPath;
+  }
+};
 
-.instructions-number {
-  padding-right: 25px;
-  font-family: "Work Sans";
-  font-size: 36px;
-  color: #00ffff;
-}
+doorImage1.onclick = () => {
+  if(currentlyPlaying && !isClicked(doorImage1)){
+  doorImage1.src = openDoor1;
+  playDoor(doorImage1);
+  } 
+};
+doorImage2.onclick = () => {
+   if(currentlyPlaying && !isClicked(doorImage2)){
+  doorImage2.src = openDoor2;
+  playDoor(doorImage2);
+  }
+};
+doorImage3.onclick = () => {
+   if(currentlyPlaying && !isClicked(doorImage3)){
+  doorImage3.src = openDoor3;
+  playDoor(doorImage3);
+  }
+};
 
-.instructions-text {
-  padding: 10px;
-  font-family: "Work Sans";
-  font-size: 14px;
-  color: #00ffff;
-}
+startButton.onclick = () => {
+  if(!currentlyPlaying) {
+    startRound();
+  }
+};
 
-.door-row {
-  text-align: center;
-}
+const startRound = () => {
+  doorImage1.src = closedDoorPath;
+  doorImage2.src = closedDoorPath;
+  doorImage3.src = closedDoorPath;
+  numClosedDoors = 3;
+  startButton.innerHTML = "Good luck!";
+  currentlyPlaying = true;
+  randomChoreDoorGenerator();
+};
 
-.door-frame {
-  cursor: pointer;
-  padding: 10px;
-}
-
-.start-row {
-  margin: auto;
-  width: 120px;
-  height: 43px;
-  font-family: "Work Sans";
-  background-color: #eb6536;
-  padding-top: 18px;
-  font-size: 18px;
-  text-align: center;
-  color: #010165;
-  margin-bottom: 21px;
-  cursor: pointer;
-}
+const gameOver = (status) => {
+  if(status == "win"){
+    startButton.innerHTML = "You win! Play again?";
+  } else {
+    startButton.innerHTML = "Game Over! Play again?";
+  }
+  currentlyPlaying = false;
+};
+startRound();
